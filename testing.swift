2d7a3d16cd6a33e74054134ca10cb207251677dd
bc-whisnant <bc.whisnant@gmail.com>
2017-12-29 10:05:50 -0500

    Added final version of functional code.
    Added more fully functional code...at this stage this part is complete.  Access to both the library and the camera works without an issue.

--- testing.swift
+++ testing.swift
@@ -1,4 +1,3 @@
-//replaced with updated code - 4:34 pm - 12/28/17
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
@@ -6,20 +5,38 @@ UINavigationControllerDelegate {
     
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        //if the device doesn't have a camera this will make sure the source type is disabled.
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    //added functionality for photo album
     @IBAction func imagePickerButtonPressed(_ sender: Any) {
         //image picker view
         let pickerController = UIImagePickerController()
+        //sets delefate for picker view
         pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
         present(pickerController, animated: true, completion: nil)
     }
     
+    //added functionaltiy for camera
+    @IBAction func cameraPickerPuttonPressed(_ sender: Any) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //sets background of imageview as long as the chosen item is a UI Image
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imagePickerView.image = pickedImage
         }
@@ -33,7 +50,11 @@ UINavigationControllerDelegate {
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print("This has been cancelled")
+        //print("This has been cancelled")
     }
+
+
+
     
+
 
